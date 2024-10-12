81e52746760b19a31da70a4cde29fc8105686482
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-12 20:39:54 +0530

    fix(userController.ts) : fix error related to return type

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,38 +1,33 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/userModel";
 
-const register = async(req:Request, res:Response, next:NextFunction) => {
+export const register = async(req:Request, res:Response, next:NextFunction) => {
     try {
         const {name, email, password, gender, mobile}:{name:string; email:string; password:string; gender:"male"|"female"|"other"; mobile:string;} = req.body;
         
-        if (!name || !email || !password || !gender || !mobile) return new Error("all fields are required");
+        if (!name || !email || !password || !gender || !mobile) {
+            res.status(400).json({success:false, message:"all fields are required"});
+            return;
+        };
         
-        const isUserExist = await User.find({email});
+        const isUserExist = await User.findOne({email});
 
-        if (isUserExist) return new Error("user already exist");
+        if (isUserExist) {
+            res.status(401).json({success:false, message:"user already exist"});
+            return;
+        };
 
-        const newUser = await User.create({
+        await User.create({
             name, email, password, gender, mobile
         });
 
         res.status(200).json({success:true, message:"User registration successfull"});
     } catch (error) {
+        let message = "";
         console.log(error);
+        if (error && typeof error === "object" && "message" in error && typeof error.message === "string" && error.message) {
+            message = error.message;
+        }
+        res.status(401).json({success:false, message:message});
     }
 };
-
-
-
-
-
-
-
-
-
-//const createUser = async(req:Request, res:Response, next:NextFunction) => {
-//    try {
-        
-//    } catch (error) {
-//        console.log(error);
-//    }
-//};
