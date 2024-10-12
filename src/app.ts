4124ba9df0c718fd1a7232826d83c1014b60e362
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-12 20:41:43 +0530

    feat(app.ts) : use userRouter in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import connectDatabase from "./config/db";
+import userRouter from "./routers/userRouter";
 
 const app = express();
 const PORT = 8000;
 
 connectDatabase();
 
-
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
     console.log("listening....");
