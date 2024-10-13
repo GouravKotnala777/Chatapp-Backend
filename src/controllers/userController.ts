d7a6b1cf4d4b0821c55680e2303bae82556dab92
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-13 14:27:09 +0530

    feat : add login controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,6 +5,9 @@ export const register = async(req:Request, res:Response, next:NextFunction) => {
     try {
         const {name, email, password, gender, mobile}:{name:string; email:string; password:string; gender:"male"|"female"|"other"; mobile:string;} = req.body;
         
+        console.log({name, email, password, gender, mobile});
+        
+
         if (!name || !email || !password || !gender || !mobile) {
             res.status(400).json({success:false, message:"all fields are required"});
             return;
@@ -22,6 +25,40 @@ export const register = async(req:Request, res:Response, next:NextFunction) => {
         });
 
         res.status(200).json({success:true, message:"User registration successfull"});
+    } catch (error) {
+        let message = "";
+        console.log(error);
+        if (error && typeof error === "object" && "message" in error && typeof error.message === "string" && error.message) {
+            message = error.message;
+        }
+        res.status(401).json({success:false, message:message});
+    }
+};
+export const login = async(req:Request, res:Response, next:NextFunction) => {
+    try {
+        const {email, password}:{email:string; password:string;} = req.body;
+        
+        console.log({email, password});
+        
+
+        if (!email || !password) {
+            res.status(400).json({success:false, message:"all fields are required"});
+            return;
+        };
+        
+        const isUserExist = await User.findOne({email});
+
+        if (!isUserExist) {
+            res.status(401).json({success:false, message:"Wrong email or password1"});
+            return;
+        };
+        
+        if (isUserExist.password !== password) {
+            res.status(401).json({success:false, message:"Wrong email or password2"});
+            return;
+        }
+
+        res.status(200).json({success:true, message:"User login successfull"});
     } catch (error) {
         let message = "";
         console.log(error);
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { register } from "../controllers/userController";
+import { login, register } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.route("/new").post(register);
+userRouter.route("/login").post(login);
 
 
 export default userRouter;
