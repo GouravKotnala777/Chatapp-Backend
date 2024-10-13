370574bce17f4ddd5c7abb367665e049045c1897
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-13 15:40:44 +0530

    feat(errorHandler.ts) : add errorHandler

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorHandler } from "../utils/ErrorHandler";
+
+
+const errorMiddleware = (err:ErrorHandler, req:Request, res:Response, next:NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    let message:string;
+
+    console.log(">>>>>>>>>>>>>>>");
+    console.log({statusCode:err.statusCode, message:err.message});
+    console.log(">>>>>>>>>>>>>>>");
+    
+    if (err.name === "CastError") {
+        message = "Wrong ObjectId format"
+    }
+    else{
+        message = err.message || "Internal server error";
+    }
+    
+    res.status(err.statusCode).json({success:false, message:err.message})
+};
+
+export default errorMiddleware;
