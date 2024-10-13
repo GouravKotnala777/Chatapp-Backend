9c1cc0ab14b8311a12dc6f006279f31ef74a30b8
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-13 13:00:41 +0530

    feat(app.ts) : configure cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import connectDatabase from "./config/db";
 import userRouter from "./routers/userRouter";
+import { config } from "dotenv";
 
 const app = express();
 const PORT = 8000;
 
+config({path:"./.env"});
+
 connectDatabase();
 
-app.use("/user", userRouter);
+app.use("/api/v1/user", userRouter);
 
 app.listen(PORT, () => {
     console.log("listening....");
