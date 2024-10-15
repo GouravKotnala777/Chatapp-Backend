d5a5ed41ea2fa2b637e2f20b50e57254474f1510
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-15 12:39:55 +0530

    feat(util.ts) : make a sendToken util function

---
+++ src/utils/util.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { Document, Schema } from "mongoose";
+import { UserTypes } from "../models/userModel";
+import { ErrorHandler } from "./ErrorHandler";
+import { cookieOptions } from "../constants/constants";
+
+
+export const sendToken = (
+    req:Request,
+    res:Response,
+    next:NextFunction,
+    model:(Document<unknown, {}, UserTypes> & UserTypes & {
+        _id: Schema.Types.ObjectId;
+    } & {
+        __v?: number;
+    })) => {
+        try {
+            const generatedToken = model.generateToken(model._id);
+        
+            if (!generatedToken) return next(new ErrorHandler("Token not generated", 500));
+        
+            res.cookie("userToken", generatedToken, cookieOptions);
+        
+            return generatedToken;
+        } catch (error) {
+            next(error);
+        }
+};
