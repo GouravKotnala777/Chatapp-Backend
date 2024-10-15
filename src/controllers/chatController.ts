628686428ea39679f26a289a7d3101ff8761291d
gouravkotnala <gouravkotnala777@gmail.com>
2024-10-15 21:42:26 +0530

    feat(chatController.ts) : add add/remove admin feature

--- src/controllers/chatController.ts
+++ src/controllers/chatController.ts
@@ -3,6 +3,7 @@ import Chat from "../models/chatModel";
 import { ErrorHandler } from "../utils/ErrorHandler";
 import Message from "../models/messageModel";
 import { AuthenticatedRequestTypes } from "../types/types";
+import mongoose from "mongoose";
 
 export const createChat = async(req:Request, res:Response, next:NextFunction) => {
     try {        
@@ -61,20 +62,64 @@ export const singleChatMessages = async(req:Request, res:Response, next:NextFunc
 };
 export const updateChat = async(req:Request, res:Response, next:NextFunction) => {
     try {
-        const {chatName, admin, members, description}:{chatName?:string; admin?:string[]; members?:string[]; description?:string;} = req.body;
+        const {chatName, members, description}:{chatName?:string; members?:string[]; description?:string;} = req.body;
         const {chatID} = req.params;
         const userID = (req as AuthenticatedRequestTypes).user._id;
 
         console.log({chatID});
 
         if (!chatID) return next(new ErrorHandler("chatID not found", 404));
-        if (!chatName && !admin && !members && !description) return next(new ErrorHandler("All fields are empty", 400));
+        if (!chatName && !members && !description) return next(new ErrorHandler("All fields are empty, there is no field for update", 400));
 
         const updatingChat = await Chat.findByIdAndUpdate(chatID, {
-            chatName, admin, members, description
+            ...(chatName&&{chatName}),
+            ...(members&&{members}),
+            ...(description&&{description})
         });
 
         res.status(200).json({success:true, message:updatingChat});
+    } catch (error) {
+        next(error);
+    }
+};
+export const addRemoveAdmin = async(req:Request, res:Response, next:NextFunction) => {
+    try {
+        const {memberID, chatID}:{memberID:mongoose.Schema.Types.ObjectId, chatID:string;} = req.body;
+        const userID = (req as AuthenticatedRequestTypes).user._id;
+
+        console.log({chatID});
+
+        if (!chatID) return next(new ErrorHandler("chatID not found", 404));
+        
+        const isChatExist = await Chat.findByIdAndUpdate(chatID);
+        
+        if (!isChatExist) return next(new ErrorHandler("Chat not exist", 404));
+
+        const findUserResult = isChatExist.admin.find((userId) => userId.toString() === userID.toString());
+        const isMemberAlreadyAnAdmin = isChatExist.admin.find((userId) => userId.toString() === memberID.toString());
+
+
+        if (findUserResult) {
+            if (isMemberAlreadyAnAdmin) {
+                const filterMemberFromAdmin = isChatExist.admin.filter((userId) => userId.toString() !== memberID.toString());
+                if (filterMemberFromAdmin.length === 0) {
+                    return next(new ErrorHandler("You have to make admin someone else, before leaving group", 400));
+                }
+                else{
+                    isChatExist.admin = filterMemberFromAdmin;       
+                }
+            }
+            else{
+                isChatExist.admin.push(memberID);    
+            }
+        }
+        else{
+            return next(new ErrorHandler("Only admin can make another user admin", 401));
+        }
+
+        await isChatExist.save();
+
+        res.status(200).json({success:true, message:"new admin added"});
     } catch (error) {
         next(error);
     }
--- src/routers/chatRouter.ts
+++ src/routers/chatRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { createChat, deleteChat, myChats, singleChatMessages, updateChat } from "../controllers/chatController";
+import { addRemoveAdmin, createChat, deleteChat, myChats, singleChatMessages, updateChat } from "../controllers/chatController";
 import { isUserAuthenticated } from "../middlewares/auth";
 
 const chatRouter = express.Router();
 
 chatRouter.route("/new").post(isUserAuthenticated, createChat);
 chatRouter.route("/my_chats").get(isUserAuthenticated, myChats);
+chatRouter.route("/add-remove-admin").put(isUserAuthenticated, addRemoveAdmin);
 chatRouter.route("/:chatID").get(isUserAuthenticated, singleChatMessages)
                             .put(isUserAuthenticated, updateChat)
                             .delete(isUserAuthenticated, deleteChat);
